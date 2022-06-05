b7d59e5ab583e4e51f8a3b133af36368559a8e18
ArkadyPol <arkims@inbox.ru>
2022-06-05 04:28:50 +0300

    change light

--- src/index.js
+++ src/index.js
@@ -1,87 +1,77 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import './index.css'
 
 const canvas = document.querySelector('#c')
 const renderer = new THREE.WebGLRenderer({ canvas })
 
-const fov = 40
+const fov = 45
 const aspect = 2
 const near = 0.1
-const far = 1000
+const far = 100
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
-camera.position.set(0, 50, 0)
-camera.up.set(0, 0, 1)
-camera.lookAt(0, 0, 0)
+camera.position.set(0, 10, 20)
+
+const controls = new OrbitControls(camera, canvas)
+controls.target.set(0, 5, 0)
+controls.update()
+
+const planeSize = 40
 
 const scene = new THREE.Scene()
 
 {
+  //light
   const color = 0xffffff
-  const intensity = 3
-  const light = new THREE.PointLight(color, intensity)
+  const intensity = 1
+  const light = new THREE.SpotLight(color, intensity)
+  light.position.set(0, 10, 0)
   scene.add(light)
+  scene.add(light.target)
 }
 
-const objects = []
+const loader = new THREE.TextureLoader()
+const texture = loader.load('assets/images/checker.png')
+texture.wrapS = THREE.RepeatWrapping
+texture.wrapT = THREE.RepeatWrapping
+texture.magFilter = THREE.NearestFilter
+const repeats = planeSize / 2
+texture.repeat.set(repeats, repeats)
 
-const radius = 1
-const widthSegments = 6
-const heightSegments = 6
-const sphereGeometry = new THREE.SphereGeometry(
-  radius,
-  widthSegments,
-  heightSegments
-)
-
-const solarSystem = new THREE.Object3D()
-scene.add(solarSystem)
-objects.push(solarSystem)
-
-const sunMaterial = new THREE.MeshPhongMaterial({ emissive: 0xffff00 })
-const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial)
-sunMesh.scale.set(5, 5, 5)
-solarSystem.add(sunMesh)
-objects.push(sunMesh)
-
-const earthOrbit = new THREE.Object3D()
-earthOrbit.position.x = 10
-solarSystem.add(earthOrbit)
-objects.push(earthOrbit)
-
-const earthMaterial = new THREE.MeshPhongMaterial({
-  color: 0x2233ff,
-  emissive: 0x112244,
+const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize)
+const planeMat = new THREE.MeshPhongMaterial({
+  map: texture,
+  side: THREE.DoubleSide,
 })
-const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial)
-earthOrbit.add(earthMesh)
-objects.push(earthMesh)
+const mesh = new THREE.Mesh(planeGeo, planeMat)
+mesh.rotation.x = Math.PI * -0.5
+scene.add(mesh)
 
-const moonOrbit = new THREE.Object3D()
-moonOrbit.position.x = 2
-earthOrbit.add(moonOrbit)
-
-const moonMaterial = new THREE.MeshPhongMaterial({
-  color: 0x888888,
-  emissive: 0x222222,
-})
-const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial)
-moonMesh.scale.set(0.5, 0.5, 0.5)
-moonOrbit.add(moonMesh)
-objects.push(moonMesh)
-
-objects.forEach(node => {
-  const axes = new THREE.AxesHelper()
-  axes.material.depthTest = false
-  axes.renderOrder = 1
-  node.add(axes)
-})
+{
+  const cubeSize = 4
+  const cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize)
+  const cubeMat = new THREE.MeshPhongMaterial({ color: '#8AC' })
+  const mesh = new THREE.Mesh(cubeGeo, cubeMat)
+  mesh.position.set(cubeSize + 1, cubeSize / 2, 0)
+  scene.add(mesh)
+}
+{
+  const sphereRadius = 3
+  const sphereWidthDivisions = 32
+  const sphereHeightDivisions = 16
+  const sphereGeo = new THREE.SphereGeometry(
+    sphereRadius,
+    sphereWidthDivisions,
+    sphereHeightDivisions
+  )
+  const sphereMat = new THREE.MeshPhongMaterial({ color: '#CA8' })
+  const mesh = new THREE.Mesh(sphereGeo, sphereMat)
+  mesh.position.set(-sphereRadius - 1, sphereRadius + 2, 0)
+  scene.add(mesh)
+}
 
 function render(time) {
   time *= 0.001
-
-  objects.forEach(obj => {
-    obj.rotation.y = time
-  })
 
   if (resizeRendererToDisplaySize(renderer)) {
     const canvas = renderer.domElement
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
-  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
+
   module: {
     rules: [
       {
@@ -12,4 +13,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './public/index.html' }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'src/assets', to: 'assets' }],
+    }),
+  ],
 }
